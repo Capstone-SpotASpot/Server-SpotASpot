359a85d2e9a9af73dd9935f978646f4d0543d5ea
Matt Rizzo <mattrizzo21@gmail.com>
2022-02-01 16:51:04 -0500

    [WIP] working sql schema creation script

--- src/database/capstone_db.sql
+++ src/database/capstone_db.sql
@@ -7,12 +7,153 @@ CREATE DATABASE IF NOT EXISTS SpotASpot_dev;
 USE SpotASpot_dev;
 
 DROP TABLE IF EXISTS readers;
+
 CREATE TABLE readers
 (
     reader_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    reader_x_coord FLOAT NOT NULL,
-    reader_y_coord FLOAT NOT NULL
+    longitude FLOAT( 10, 6 ) NOT NULL,
+    latitude FLOAT( 10, 6 ) NOT NULL
 );
 
-INSERT INTO readers (reader_x_coord, reader_y_coord)
+-- Create recursive reader relationship
+DROP TABLE IF EXISTS adjacent_readers;
+CREATE TABLE adjacent_readers
+(
+    adjacency_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+
+    -- Pairs 2 readers together if they are in range of each other.
+    -- The order does NOT matter
+    reader_one INT NOT NULL,
+    reader_two INT NOT NULL,
+
+    -- the readers are FK to the actual reader table
+    CONSTRAINT FK_reader_one
+        FOREIGN KEY (reader_one) REFERENCES readers(reader_id)
+        ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT FK_reader_two
+        FOREIGN KEY (reader_two) REFERENCES readers(reader_id)
+        ON UPDATE CASCADE ON DELETE CASCADE
+
+);
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users
+(
+    user_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    lib_password VARCHAR(50) NOT NULL
+);
+
+-- Need the tag table so it can be a FK in the car table
+DROP TABLE IF EXISTS tag;
+CREATE TABLE tag
+(
+    tag_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    car_pos ENUM('front', 'rear', 'middle')
+);
+
+DROP TABLE IF EXISTS registered_cars;
+CREATE TABLE registered_cars
+(
+    car_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    front_tag INT NOT NULL,
+    middle_tag INT NOT NULL,
+    rear_tag INT NOT NULL,
+    registering_user INT NOT NULL,
+
+    -- tags are FK to the tag table
+    CONSTRAINT front_tag_fk
+        FOREIGN KEY (front_tag)
+        REFERENCES tag(tag_id)
+        ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT middle_tag_fk
+        FOREIGN KEY (middle_tag)
+        REFERENCES tag(tag_id)
+        ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT rear_tag_fk
+        FOREIGN KEY (rear_tag)
+        REFERENCES tag(tag_id)
+        ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT registering_user_fk
+        FOREIGN KEY (registering_user)
+        REFERENCES users(user_id)
+        ON UPDATE RESTRICT ON DELETE CASCADE
+);
+
+
+DROP TABLE IF EXISTS parking_spot;
+CREATE TABLE parking_spot
+(
+    spot_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    longitude FLOAT( 10, 6 ) NOT NULL,
+    latitude FLOAT( 10, 6 ) NOT NULL,
+
+    -- allowed to be null when no car is parked
+    time_since_parked TIMESTAMP,
+    parked_car_id INT NULL,
+
+    CONSTRAINT parked_car_fk
+        FOREIGN KEY (parked_car_id)
+        REFERENCES registered_cars (car_id)
+        ON UPDATE CASCADE ON DELETE SET NULL
+);
+
+DROP TABLE IF EXISTS observation_event;
+CREATE TABLE observation_event
+(
+    observation_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    time_observed DATETIME NOT NULL,
+    signal_strength FLOAT NOT NULL,
+
+    -- Used to keep track of old events that no longer factor into algo's
+    is_relevant boolean NOT NULL
+);
+
+-- Association Tables
+
+-- association between parking_spot and reader tables
+DROP TABLE IF EXISTS reader_coverage;
+CREATE TABLE reader_coverage
+(
+    coverage_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    covering_reader_id INT NOT NULL,
+    spot_covered_id INT NOT NULL,
+
+    -- create FK for association table relationships
+    CONSTRAINT reader_cover_fk
+        FOREIGN KEY (covering_reader_id)
+        REFERENCES readers(reader_id)
+        ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT covered_spot_fk
+        FOREIGN KEY (spot_covered_id)
+        REFERENCES parking_spot(spot_id)
+        ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+-- associate reader-tag-observation_event
+DROP TABLE IF EXISTS detects;
+CREATE TABLE detects
+(
+    detection_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    detecting_reader_id INT NOT NULL,
+    detected_tag_id INT NOT NULL,
+    observation_event_id INT NOT NULL,
+
+    CONSTRAINT detects_reader_fk
+        FOREIGN KEY (detecting_reader_id)
+        REFERENCES readers (reader_id)
+        ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT detects_tag_fk
+        FOREIGN KEY (detected_tag_id)
+        REFERENCES tag (tag_id)
+        ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT detects_event_fk
+        FOREIGN KEY (observation_event_id)
+        REFERENCES observation_event (observation_id)
+        ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+INSERT INTO readers (longitude, latitude)
     VALUES (0.0, 0.0), (1.0, 1.0)
