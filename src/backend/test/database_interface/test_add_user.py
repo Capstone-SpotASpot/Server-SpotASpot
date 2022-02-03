10371b48fa4744bcc1230dab253e726bcdfa5db8
Matt Rizzo <mattrizzo21@gmail.com>
2022-02-02 19:52:09 -0500

    test add script works

--- src/backend/test/database_interface/test_add_user.py
+++ src/backend/test/database_interface/test_add_user.py
@@ -1,12 +1,46 @@
+from sre_constants import FAILURE
 import pymysql
 from pymysql import connections, cursors
+from pymysql.cursors import Cursor
 
 
-def _cleanup_test(conn: connections, cursor: cursors):
-    pass
+#--------------------------------Project Includes--------------------------------#
+from db_manager import DB_Manager
 
-def test_add_user(conn: connections, cursor: cursors):
+def test_add_user(db_manager: DB_Manager) -> bool:
     print("Running test for add_user procedure")
-    _cleanup_test(conn, cursor)
+    did_pass = True
+    first_name = "test_fname"
+    last_name = "test_lname"
+    username = "test_username"
+    pwd = "test_pwd"
+    filter_str = []
+    filter_str.append(f"first_name = '{first_name}'")
+    filter_str.append(f"AND last_name = '{last_name}'")
+    filter_str.append(f"AND username = '{username}'")
+    filter_str.append(f"AND user_password = '{pwd}'")
+
+    did_pass &= db_manager.add_user(first_name, last_name, username, pwd)
+    print(f"did_pass after add user = {did_pass}")
+    # Check to see that there is a row in the table which matches the inputs
+    try:
+        filter_clause = " ".join(filter_str)
+        db_manager.cursor.execute(f"select * from users WHERE {filter_clause};")
+    except:
+        did_pass = False
+
+    # Cleanup test by deleting the newly made row
+    try:
+        filter_clause = " ".join(filter_str[:-1])
+        print(f"fitler for delete {filter_clause}")
+        db_manager.cursor.execute(f"DELETE FROM users WHERE {filter_clause};")
+        db_manager.conn.commit()
+        res = db_manager.cursor.fetchall()
+    except:
+        did_pass = False
+
+    print(f"test pass? {did_pass}")
+    # assert(did_pass)
+
 
 
