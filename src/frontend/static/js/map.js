985f81f58b08422691dd3513444c3c99edd70efb
Matt Rizzo <mattrizzo21@gmail.com>
2022-04-12 17:00:44 -0400

    [refac] to use JSON for spots instead of array

--- src/frontend/static/js/map.js
+++ src/frontend/static/js/map.js
@@ -7,6 +7,18 @@ $(document).ready(async function() {
 });
 
 
+const icons = {
+    taken: {
+        icon: "/static/images/parking_spot_taken.png"
+    },
+    free: {
+        icon: "/static/images/parking_spot_free.png"
+    }
+};
+
+// key = spot id
+const parking_spots = {};
+
 export const initMap = async () => {
     const coords = await get_gps_coords();
     console.log(`my (lat, long): (${coords.lat}, ${coords.long})`)
@@ -40,54 +52,49 @@ const create_map = async (lat, long) => {
 
     map.set('styles', custom_style);
 
-
-  const iconBase =
-    "https://developers.google.com/maps/documentation/javascript/examples/full/images/";
-
-    const icons = {
-        taken: {
-            icon: "/static/images/parking_spot_taken.png"
-        },
-        free: {
-            icon: "/static/images/parking_spot_free.png"
-        }
-    };
-
     // NOTE: radius is large, but on day of pres can make it small around Ell Hall
     const get_reader_url = `/mobile/get_local_readers?radius=${10000}&latitude=${lat}&longitude=${long}`;
     const readers = await async_get_request(get_reader_url, {});
 
-
-
-    const parking_spots = [];
+    // const parking_spots = [];
     for (const reader of readers)
     {
         const get_spot_status_url = `/mobile/get_is_spot_taken/${reader.reader_id}`
         const spot_statuses = await async_get_request(get_spot_status_url, {});
 
 
-        for(const spot of Object.values(spot_statuses))
+        for(const [spot_id, spot] of Object.entries(spot_statuses))
         {
             // process each spot seen by a reader
             const type = spot.is_spot_taken ? "taken" : "free";
             const cur_marker = {
                 position: new google.maps.LatLng(spot.latitude, spot.longitude),
-                type: type
+                type: type,
             }
-            parking_spots.push(cur_marker);
+            // parking_spots.push(cur_marker);
+            parking_spots[spot_id] = cur_marker;
         }
     }
 
     // actually generate the markers
-    for(const feat of parking_spots)
+    for(const spot of Object.values(parking_spots))
     {
         const marker = new google.maps.Marker({
-            position: feat.position,
-            icon: icons[feat.type].icon,
+            position: spot.position,
+            icon: icons[spot.type].icon,
             map: map,
         })
     };
 
+}
+
+/**
+ * @brief Given a JSON of parking spots statuses from the /mobile/get_is_spot_taken route,
+ * updates the markers on the map
+ * @param {Array} parking_spots The parking spots to check the status of
+ */
+const update_markers = async (parking_spots) =>
+{
 }
 
 /**
